a37a04c397983143c8d4242b371aa39e4b4fa6fc
Srihari <ysautomator@gmail.com>
2025-11-16 14:13:48 +0530

    added github reporter

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwrightw/);
 });
 
 test('get started link', async ({ page }) => {
@@ -17,4 +17,3 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-  
